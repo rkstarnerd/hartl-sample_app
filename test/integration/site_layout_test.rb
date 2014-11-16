417a715d7eba9bb9df743d3082b24630d0a89e88
rkstarnerd <gemille.ford@gmail.com>
2014-11-16 12:16:50 -0500

    Modifies layout test and home page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,9 +8,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
    test "layout links" do
     get root_path
     assert_template 'static_pages/home'
+
+    get signup_path
+    assert_template 'users/new'
+
+    get help_path
+    assert_template 'help'
+
+    get about_path
+    assert_template 'about'
+
+    get contact_path
+    assert_template 'contact'
+
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
   end
 end
