14cd49a2237d7cb588f28b9919dac3e98dbb0fa1
rkstarnerd <gemille.ford@gmail.com>
2014-11-17 01:52:50 -0500

    Validates password and confirmation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -33,7 +33,7 @@ class UserTest < ActiveSupport::TestCase
     valid_addresses = %w[user@example.com USER@FOO.com A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
-      assert @user.valid?, "#{valid_address.inspect} should e valid"
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
 
@@ -50,5 +50,10 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should not be too small" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
