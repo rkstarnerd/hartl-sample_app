fcca384e88fb0443d5b9fd7f532bdbfd4c42d683
rkstarnerd <gemille.ford@gmail.com>
2014-11-19 19:39:04 -0500

    Finds the current user in the session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  #Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #Returns the curent logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: sesssion[:user_id])
+  end
 end
