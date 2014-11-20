5e9804a155a81f20cdd4a80a23d086bf700b2884
rkstarnerd <gemille.ford@gmail.com>
2014-11-19 19:09:13 -0500

    Adds log in page and error message for invalid log in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
   #Returns the Gravatar for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, *size)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size.join("")}"
     image_tag(gravatar_url, alt: user.name, classs: "gravatar")
   end
 end
