3b50efa2de6a20897fc3725b99422598cff78026
rkstarnerd <gemille.ford@gmail.com>
2014-11-20 18:58:26 -0500

    Creates the logged_in? helper method and updates layout for logged-in users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,4 +8,8 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: sesssion[:user_id])
   end
+
+  #Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
 end
