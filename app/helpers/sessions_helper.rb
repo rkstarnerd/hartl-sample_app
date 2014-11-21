8d8e46f9b469102b6bed5d236b7b3d5d671ea3f9
rkstarnerd <gemille.ford@gmail.com>
2014-11-20 19:06:06 -0500

    Corrects errors

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,10 +6,11 @@ module SessionsHelper
 
   #Returns the curent logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: sesssion[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   #Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
 end
