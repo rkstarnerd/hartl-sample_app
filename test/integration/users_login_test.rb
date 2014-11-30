6144ff50c600802d45d6d4ea007bfa6d711d462a
rkstarnerd <gemille.ford@gmail.com>
2014-11-30 06:08:14 -0500

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:gemille)
   end
 
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: "", password: "" }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test "login with valid information followed by logout" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
