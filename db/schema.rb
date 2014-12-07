5c0a84f9060c7f66a9a375dab41aeda588fe2719
rkstarnerd <gemille.ford@gmail.com>
2014-12-07 01:56:24 -0500

    Add password resets and email configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205012801) do
+ActiveRecord::Schema.define(version: 20141207055554) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141205012801) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
